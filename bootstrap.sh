8e4d377412aac7afc92aaac32e8c7c322a6026d9
Yuriy Lyutov <delyshdesign@gmail.com>
2019-02-28 21:12:01 +0100

    Adding php7.1-sqlite3 and php7.1-xml packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,7 @@ sudo apt install -y apache2
 #sudo apt-get remove -y php*
 
 # installing php7.1 and php modules
-sudo apt-get install -y php7.1 php7.1-cli php7.1-common libapache2-mod-php7.1 php7.1-fpm php7.1-curl php7.1-gd php7.1-bz2 php7.1-mcrypt php7.1-json php7.1-tidy php7.1-mbstring php-redis php-memcached
+sudo apt-get install -y php7.1 php7.1-cli php7.1-common libapache2-mod-php7.1 php7.1-fpm php7.1-curl php7.1-gd php7.1-bz2 php7.1-mcrypt php7.1-json php7.1-tidy php7.1-mbstring php-redis php-memcached php7.1-sqlite3 php7.1-xml
 
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
